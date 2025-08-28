64889c49267de6912fd356b9e0ba68ffcf353e2a
ilarao <ilakaurrao@gmail.com>
2025-08-27 19:01:10 -0700

    Update encoderAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderAuto.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@Autonomous
 public class encoderAuto extends LinearOpMode {
 
     @Override
